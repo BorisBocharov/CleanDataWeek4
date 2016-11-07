c35fb9d4c3f13c337af56947818585fdc77f0ca8
BorisBocharov <BorisBocharov@UmpquaBank.com>
2016-11-07 15:14:13 -0800

    Updated run_analysis.R
    Updated the script to only include mean() and std() variables in the tidy data set.

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 # Preliminary
 
 # Set working directory to UCI HAR Dataset folder
-# setwd("C:/Users/BorisBocharov/Desktop/Data Science Coursera/Data Cleaning/Week 4/Project/UCI HAR Dataset")
+setwd("C:/Users/BorisBocharov/Desktop/Data Science Coursera/Data Cleaning/Week 4/Project")
 #getwd()
 
 #Fetch descriptive names of variables and activities
@@ -9,9 +9,6 @@ filename <- "./UCI HAR Dataset/features.txt"
 var_names <- read.table(filename,header=FALSE, sep="")
 filename <- "./UCI HAR Dataset/activity_labels.txt"
 Act_names <- read.table(filename,header=FALSE, sep="")
-
-#re-name activities
-Act_names[,2] <- c("Walk", "Walk_Up", "Walk_Dn", "Sit", "Stand", "Lay")
 
 # Create Train Dataset 
 
@@ -30,7 +27,7 @@ dim(SubjectID_train)[1]
 
 # Merge and name
 Data_train <- cbind(X_train, y_train, SubjectID_train)
-colnames(Data_train) <- c(as.character(var_names[,2]),"Response", "Subject")
+colnames(Data_train) <- c(as.character(var_names[,2]),"Activity", "Subject")
 
 # Create Test Dataset 
 
@@ -49,7 +46,7 @@ dim(SubjectID_test)[1]
 
 # Merge and name
 Data_test <- cbind(X_test, y_test, SubjectID_test)
-colnames(Data_test) <- c(as.character(var_names[,2]),"Response", "Subject")
+colnames(Data_test) <- c(as.character(var_names[,2]),"Activity", "Subject")
 
 
 # STEP 1. Combine Train & Test Datasets
@@ -64,11 +61,15 @@ dim(X_train)[1] + dim(X_test)[1]
 
 # STEP 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 # These contain mean() and std()
-Data_All_mean_n_std <- Data_All[ ,union(grep("mean()", colnames(Data_All)),grep("std()", colnames(Data_All)))]
+a1 <- grep("mean()", colnames(Data_All))
+a2 <- grep("std()", colnames(Data_All))
+a3 <- grep("Activity", colnames(Data_All))
+a4 <- grep("Subject", colnames(Data_All))
+Data_All <- Data_All[ ,c(a1,a2,a3,a4)]
 
 # STEP 3. Uses descriptive activity names to name the activities in the data set.
-Data_All <- inner_join(Data_All, Act_names, by = c("Response" = "V1"))
-colnames(Data_All)[length(colnames(Data_All))] <- "Response_Desc"
+Data_All <- inner_join(Data_All, Act_names, by = c("Activity" = "V1"))
+colnames(Data_All)[length(colnames(Data_All))] <- "Activity_Desc"
 
 # STEP 4. Appropriately labels the data set with descriptive variable names.
 # Done at Train/Test data sets creation stage.
@@ -78,10 +79,13 @@ colnames(Data_All)[length(colnames(Data_All))] <- "Response_Desc"
 temp <- length(colnames(Data_All))
 
 # Melt the Data_All dataset using Response (Activity), Response_Desc and Subject as variables
-Data_All_molten <- melt(Data_All, id = c(temp-2, temp,temp-1))
+Data_All_molten <- melt(Data_All, id = c((temp-2):temp))
 
 # Re-cast the molten dataset by averaging each variable (for each Response/Subject combination)
-Data_All_reshaped <- dcast(Data_All_molten, Response + Response_Desc + Subject ~ variable, mean)
+Data_All_reshaped <- dcast(Data_All_molten, Subject + Activity + Activity_Desc ~ variable, mean)
+
+#Remove activity (Id)
+Data_All_reshaped <- Data_All_reshaped[,-2]
 
 # Save tidy dataset to working directory
-write.table(Data_All_reshaped, file = "Tidy_dataset.txt", row.names = FALSE, sep="")
+write.table(Data_All_reshaped, file = "Tidy_dataset.txt", row.names = FALSE)
